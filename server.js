2fa886a4c0e4026b0cd6f5a547ff27ca31d1a523
Divine Obido <divineobido64@gmail.com>
2025-07-11 23:16:41 +0100

    Adjuested port to be accessible from any network

--- server.js
+++ server.js
@@ -57,6 +57,6 @@ app.delete('/todos/:id', (req, res) => {
   res.status(204).send();
 });
 
-app.listen(PORT, () => {
-  console.log(`✅ Server running on http://127.0.0.1:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`✅ Server running on http://0.0.0.0:${PORT}`);
 });
